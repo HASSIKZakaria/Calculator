f920a3ef0287c271c8bec27736528562760e9d9b
HASSIKZakaria <hassik.zakaria@gmail.com>
2024-12-20 10:25:43 +0100

    Initial commit

---
+++ src/main/java/com/zakaria/calculator/Calculator.java
@@ -0,0 +1,6 @@
+package com.zakaria.calculator;
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,15 @@
+package com.zakaria.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAddition() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(2, 3);
+        assertEquals(5, result, "2 + 3 doit être égal à 5");
+    }
+}
