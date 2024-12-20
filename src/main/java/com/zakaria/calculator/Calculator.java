73aa39a60b8951bfc912e34d602c6869974bdfb2
HASSIKZakaria <hassik.zakaria@gmail.com>
2024-12-20 11:28:03 +0100

    ajout des tsts pour les méthodes add et divide

--- src/main/java/com/zakaria/calculator/Calculator.java
+++ src/main/java/com/zakaria/calculator/Calculator.java
@@ -3,4 +3,7 @@ public class Calculator {
     public int add(int a, int b) {
         return a + b;
     }
+    public int divide(int opG, int opD) {
+        return opG / opD ;
+    }
 }
--- src/test/java/com/zakaria/calculator/CalculatorTest.java
+++ src/test/java/com/zakaria/calculator/CalculatorTest.java
@@ -4,16 +4,17 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class CalculatorTest {
+class CalculatorTest {
 
     @Test
-    public void testAddition() {
+    void add() {
         Calculator calculator = new Calculator();
         int result = calculator.add(2, 3);
         assertEquals(5, result, "2 + 3 doit être égal à 5");
     }
+
     @Test
-    public void testdiv() {
+    void divide() {
         Calculator calculator = new Calculator();
         int result = calculator.divide(6, 3);
         assertEquals(2, result, "6 / 3 doit être égal à 2");
